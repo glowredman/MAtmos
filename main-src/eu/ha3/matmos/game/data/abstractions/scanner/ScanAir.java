34f4c0c89516b3b21518ff84f1f627d35b4993d3
makamys <makamys@outlook.com>
2020-02-06 06:28:11 +0100

    Clean up stuff, remove some debug prints

--- main-src/eu/ha3/matmos/game/data/abstractions/scanner/ScanAir.java
+++ main-src/eu/ha3/matmos/game/data/abstractions/scanner/ScanAir.java
@@ -32,7 +32,8 @@ public class ScanAir extends Scan {
 	enum Stage {AIR1, SOLID, AIR2, FINISH};
 	Stage stage = Stage.AIR1;
 	
-	private final int AIR_COST = 1, SOLID_COST = 2;
+	private int AIR_COST, SOLID_COST, PANE_COST; // these are meant to be final, but for ease of development are non-final for now(?)
+	byte START_NEARNESS;
 	
 	@Override
 	void initScan(int x, int y, int z, int xsizeIn, int ysizeIn, int zsizeIn, int opspercallIn)
@@ -45,7 +46,7 @@ public class ScanAir extends Scan {
 		yy = 0;
 		zz = 0;
 		
-		final byte START_NEARNESS = 15;
+		
 		
 		int newScanDistance = Math.min(Math.min((xsizeIn - 1) / 2, (ysizeIn - 1) / 2), (zsizeIn - 1) / 2);
 		
@@ -78,10 +79,15 @@ public class ScanAir extends Scan {
 			finalQueue.clear();
 		}
 		
-		floodQueue.push4((byte)scanDistance, (byte)scanDistance, (byte)scanDistance, /*START_NEARNESS*/(byte)4);
+		floodQueue.push4((byte)scanDistance, (byte)scanDistance, (byte)scanDistance, START_NEARNESS);
 		
 		nearest = -1;
 		stage = Stage.AIR1;
+		
+		AIR_COST = 1;
+		SOLID_COST = 2;
+		PANE_COST = 2;
+		START_NEARNESS = 4;
 		
 		finalProgress = 1; // TODO proper progress tracking. for now it just sets it to 1 when finished
 	}
@@ -103,17 +109,11 @@ public class ScanAir extends Scan {
 	@Override
 	protected boolean doRoutine()
 	{
-		System.out.println("doRoutine()");
 		int ops = 0;
-		
-		
 		
 		int scanSize = scanDistance * 2 + 1;
 		
 		World w = Minecraft.getMinecraft().theWorld;
-		
-		int airs = 0;
-		int solids = 0;
 		
 		byte[] p = new byte[4];
 		while(ops < opspercall && progress < finalProgress)
@@ -122,7 +122,6 @@ public class ScanAir extends Scan {
 			case AIR1:
 			case AIR2:
 				if(floodQueue.pop4(p)) {
-					//System.out.println(String.format("air1 step (%d, %d, %d, %d)", p[0], p[1], p[2], p[3]));
 					int wx = startX - scanDistance + p[0];
 					int wy = startY - scanDistance + p[1];
 					int wz = startZ - scanDistance + p[2];
@@ -139,7 +138,6 @@ public class ScanAir extends Scan {
 						
 						byte newN = (byte) (p[3] - AIR_COST);
 						if(block instanceof BlockAir) {
-							airs++;
 							if(w.canBlockSeeTheSky(wx, wy, wz)) {
 								if(nearest == -1 || p[3] > nearest) {
 									nearest = p[3];
@@ -150,8 +148,7 @@ public class ScanAir extends Scan {
 									finalQueue.push4(p[0], p[1], p[2], p[3]);
 								}
 							} else {
-								// enqueue neighbors
-								System.out.println(String.format("    enqueue neighbors (%d, %d, %d, %d)", p[0], p[1], p[2], p[3]));
+								// enqueue neighbor
 								floodQueue.push4((byte)(p[0] - 1), 	p[1], 				p[2],				newN);
 								floodQueue.push4((byte)(p[0] + 1), 	p[1],				p[2],				newN);
 								floodQueue.push4(p[0], 				(byte)(p[1] - 1),	p[2],				newN);
@@ -160,8 +157,6 @@ public class ScanAir extends Scan {
 								floodQueue.push4(p[0], 				p[1], 				(byte)(p[2] + 1),	newN);
 							}
 						} else {
-							solids++;
-							System.out.println(String.format("    enqueue solid (%d, %d, %d, %d)", p[0], p[1], p[2], p[3]));
 							(stage == Stage.AIR1 ? solidQueue : finalQueue).push4(p[0], p[1], p[2], p[3]);
 						}
 						
@@ -181,7 +176,7 @@ public class ScanAir extends Scan {
 					if(MAtmosUtility.isWithinBounds(wy)) {
 						
 						// enqueue neighbors
-						byte newN = (byte)(p[3] - /*SOLID_COST*/2);
+						byte newN = (byte)(p[3] - SOLID_COST);
 						
 						floodQueue.push4((byte)(p[0] - 1), 	p[1], 				p[2],				newN);
 						floodQueue.push4((byte)(p[0] + 1), 	p[1],				p[2],				newN);
@@ -201,26 +196,10 @@ public class ScanAir extends Scan {
 				// goal of outsourcing scanning it to us in scan_large's stead, avoiding scanning it twice.
 				// But I'm not sure if it's worth the trouble for the optimization gain.
 				
-				System.out.println("end. nearest: " + nearest);
-				System.out.println(String.format("%d airs, %d solids", airs, solids));
+				System.out.println("outdoor nearness: " + nearest);
 				progress = 1;
 				break;
 			}
-			
-			/*if(MAtmosUtility.isWithinBounds(yy)) {
-				pipeline.input(startX - scanDistance + xx, startY - scanDistance + yy, startZ - scanDistance + zz);
-			}
-			xx = (xx + 1) % scanSize;
-			if(xx == 0) {
-				zz = (zz + 1) % scanSize;
-				if(zz == 0) {
-					yy = yy + 1;
-					if(yy == scanSize) {
-						progress = 1;
-					}
-				}
-			}
-			ops++;*/
 		}
 		
 		return true;
