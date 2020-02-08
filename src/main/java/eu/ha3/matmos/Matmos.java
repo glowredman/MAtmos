14e8aafd404fd49ba11d70fabae33bf9e70d7ad6
makamys <makamys@outlook.com>
2020-02-08 22:18:00 +0100

    Increase sound system streaming channels to 11 (from 4)
    When travelling between different machines, the loops kept dying.
    It seems that when a stream is played when all streaming channels
    are already playing, one of them gets interrupted without any
    notification, resulting in a zombie NoAttenuationMovingSound that
    gets stuck thinking it's still playing while it's actually not.
    
    This is just a quick fix, but one that should work for a while,
    since it's unlikely I'd reach over 11 simultaneous streams.

--- src/main/java/eu/ha3/matmos/Matmos.java
+++ src/main/java/eu/ha3/matmos/Matmos.java
@@ -79,6 +79,9 @@ public class Matmos extends HaddonImpl implements SupportsFrameEvents, SupportsT
 
     @Override
     public void onLoad() {
+        SoundSystemConfig.setNumberStreamingChannels(11);
+        SoundSystemConfig.setNumberNormalChannels(32-11);
+        
         this.<OperatorCaster>op().setTickEnabled(true);
         this.<OperatorCaster>op().setFrameEnabled(true);
 
