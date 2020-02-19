a480aa7ffc4701be49df4cd0533b15ac444fd07a
makamys <makamys@outlook.com>
2020-02-19 20:51:40 +0100

    Add 4 fields to ModuleWorld
    * can_snow_here
    * biome_is_snowy
    * biome_temperature
    * biome_rainfall

--- src/main/java/eu/ha3/matmos/data/modules/world/ModuleWorld.java
+++ src/main/java/eu/ha3/matmos/data/modules/world/ModuleWorld.java
@@ -6,7 +6,9 @@ import eu.ha3.matmos.data.modules.ModuleProcessor;
 import eu.ha3.matmos.util.MAtUtil;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
 import net.minecraft.world.storage.WorldInfo;
 
 public class ModuleWorld extends ModuleProcessor implements Module {
@@ -19,7 +21,9 @@ public class ModuleWorld extends ModuleProcessor implements Module {
         World w = Minecraft.getMinecraft().world;
         EntityPlayer player = getPlayer();
         WorldInfo info = w.getWorldInfo();
-
+        BlockPos pos = MAtUtil.getPlayerPos();
+        Biome biome = w.getBiome(pos);
+        
         setValue("time_modulo24k", (int)(info.getWorldTime() % 24000L));
         setValue("rain", w.isRaining());
         setValue("thunder", info.isThundering());
@@ -28,8 +32,12 @@ public class ModuleWorld extends ModuleProcessor implements Module {
         setValue("light_subtracted", w.getSkylightSubtracted());
         setValue("remote", w.isRemote);
         setValue("moon_phase", w.getMoonPhase());
-        setValue("can_rain_on", w.canSeeSky(MAtUtil.getPlayerPos()));
-        setValue("biome_can_rain", w.getBiome(MAtUtil.getPlayerPos()).canRain());
+        setValue("can_rain_on", w.canSeeSky(pos));
+        setValue("can_snow_here", w.canSnowAt(pos, false));
+        setValue("biome_can_rain", biome.canRain());
+        setValue("biome_is_snowy", biome.isSnowyBiome());
+        setValue("biome_temperature", Math.round(biome.getTemperature(pos) * 1000));
+        setValue("biome_rainfall", Math.round(biome.getRainfall() * 1000));
         setValue("rain_force1k", Math.round(w.getRainStrength(0f) * 1000));
         setValue("thunder_force1k", Math.round(w.getThunderStrength(0f) * 1000));
 
