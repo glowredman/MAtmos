415a706e8049395c977e6edefe96789833b1c28f
makamys <makamys@outlook.com>
2020-02-29 17:14:13 +0100

    Fix streams getting stuck playing when switching worlds on LiteLoader

--- src/main/java/eu/ha3/matmos/core/mixin/MixinSoundManager.java
+++ src/main/java/eu/ha3/matmos/core/mixin/MixinSoundManager.java
@@ -1,10 +1,14 @@
 package eu.ha3.matmos.core.mixin;
 
+import java.util.List;
+
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import eu.ha3.matmos.Matmos;
 import eu.ha3.matmos.core.ducks.ISoundManager;
 import net.minecraft.client.audio.SoundManager;
 import paulscode.sound.SoundSystem;
@@ -15,12 +19,29 @@ abstract class MixinSoundManager implements ISoundManager {
 
     private SoundSystem __sndSystem;
     
+    @Shadow
+    private List<String> pausedChannels;
+    
+    @Shadow
+    private boolean loaded;
+    
     @Inject(method = "<init>*", at = @At("RETURN"))
     private void onConstructed(CallbackInfo ci) {
-        System.out.println("SoundManger mixin onConstructed!");
+        Matmos.LOGGER.debug("Running mixin for SoundManager constructor!");
         SoundSystemConfig.setNumberStreamingChannels(11);
         SoundSystemConfig.setNumberNormalChannels(32-11);
         SoundSystemConfig.setStreamQueueFormatsMatch(true);
+    }
+    
+    /** Applies Forge's fix for MC-35856. Required for LiteLoader. Redundant if we're using Forge, but should be harmless. */
+    @Inject(method = "stopAllSounds", at = @At("RETURN"))
+    public void stopAllSounds(CallbackInfo ci)
+    {
+        Matmos.LOGGER.debug("Running mixin for SoundManager.stopAllSounds!");
+        if (this.loaded)
+        {
+            this.pausedChannels.clear(); //Forge: MC-35856 Fixed paused sounds repeating when switching worlds
+        }
     }
 
     @Override
