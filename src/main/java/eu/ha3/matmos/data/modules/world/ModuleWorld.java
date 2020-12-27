3a7e9355bda7beadd6274373bbe3f8ee4fc4b142
makamys <makamys@outlook.com>
2020-12-27 21:38:55 +0100

    Calculate time_modulo24k from celestial angle
    This adds compatibility with TooMuchTime.

--- src/main/java/eu/ha3/matmos/data/modules/world/ModuleWorld.java
+++ src/main/java/eu/ha3/matmos/data/modules/world/ModuleWorld.java
@@ -9,15 +9,30 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import eu.ha3.matmos.util.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldProviderSurface;
 import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraft.world.storage.WorldInfo;
 
 public class ModuleWorld extends ModuleProcessor implements Module {
     float rainStrengthThreshold;
     
+    int[] celestialAngleToTime = constructCelestialAngleToTimeTable();
+    
     public ModuleWorld(DataPackage data) {
         super(data, "w_general");
         rainStrengthThreshold = ConfigManager.getConfig().getFloat("rain.strengththreshold");
+    }
+    
+    private static int[] constructCelestialAngleToTimeTable() {
+        int[] angleToTime = new int[1000];
+        
+        WorldProviderSurface p = new WorldProviderSurface();
+        for(int t = 23999; t >= 0; t--) {
+            float angle = p.calculateCelestialAngle(t, 0);
+            angleToTime[(int)(angle * angleToTime.length)] = t;
+        }
+        
+        return angleToTime;
     }
 
     @Override
@@ -28,7 +43,7 @@ public class ModuleWorld extends ModuleProcessor implements Module {
         BlockPos pos = MAtUtil.getPlayerPos();
         BiomeGenBase biome = w.getBiomeGenForCoords(pos.getX(), pos.getZ());
 
-        setValue("time_modulo24k", (int) (info.getWorldTime() % 24000L));
+        setValue("time_modulo24k", invCalculateCelestialAngle(w.provider.calculateCelestialAngle(info.getWorldTime(), 0)));
         setValue("rain", rainStrengthThreshold == -1 ? w.isRaining() 
                 : w.getRainStrength(0f) > rainStrengthThreshold);
         setValue("thunder", info.isThundering());
@@ -51,5 +66,23 @@ public class ModuleWorld extends ModuleProcessor implements Module {
         setValue("rain_force1k", Math.round(w.getRainStrength(0f) * 1000));
         setValue("thunder_force1k", Math.round(w.getWeightedThunderStrength(0f) * 1000));
 
+    }
+    
+    private int invCalculateCelestialAngle(float x) {
+        /* Approximation of the inverse of calculateCelestialAngle, with a max error of 11 */
+        
+        x = x % 1f;
+        
+        int prevIndex = (int)(x * celestialAngleToTime.length);
+        float alpha = ((x * celestialAngleToTime.length) - (float)Math.floor(x * celestialAngleToTime.length));
+        int nextIndex = prevIndex + 1 < celestialAngleToTime.length ? prevIndex + 1 : 0;
+        
+        int prev = celestialAngleToTime[prevIndex];
+        int next = celestialAngleToTime[nextIndex];
+        if(next < prev) {
+            next = 24000;
+        }
+        
+        return (int)((1f - alpha) * prev + alpha * next);
     }
 }
