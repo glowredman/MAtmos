9148637294322dd323df1fa28b94885d48bc0817
makamys <makamys@outlook.com>
2020-12-29 07:03:36 +0100

    Revamp alias file parser to support importing and fix colon handling

--- src/main/java/eu/ha3/matmos/util/IDDealiaser.java
+++ src/main/java/eu/ha3/matmos/util/IDDealiaser.java
@@ -7,13 +7,18 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
 import java.util.stream.IntStream;
+
+import org.apache.commons.io.IOUtils;
 
 import eu.ha3.matmos.ConfigManager;
 import eu.ha3.matmos.Matmos;
+import eu.ha3.matmos.core.sheet.DataPackage;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
@@ -29,23 +34,67 @@ public class IDDealiaser {
 
         compile();
     }
+    
+    private Map<String, String> loadEntries(Map<String, String> entries, String aliasDir, String path, Set<String> visited){
+        try (FileReader reader = new FileReader(new File(aliasDir, path))) {
+            int lineno = 0;
+            for(String line : IOUtils.readLines(reader)) {
+                line = line.trim();
+                if(line.startsWith("#")|| line.equals("")) {
+                    // do nothing
+                } else if(line.startsWith(":")) {
+                    String[] words = line.split(" ");
+                    String directive = words[0].substring(1);
+                    String argument = words[1];
+                    
+                    if(directive.equals("import")) {
+                        if(!visited.contains(argument)){
+                            loadEntries(entries, aliasDir, argument, visited);
+                        } else {
+                            Matmos.LOGGER.warn(String.format("%s:%d: Import cycle detected (%s->...->%s->%s)",
+                                    path, lineno, argument, path, argument));
+                        }
+                    } else {
+                        Matmos.LOGGER.warn(String.format("%s:%d: Invalid directive: %s", path, lineno, argument));
+                    }
+                } else {
+                    String[] sides = line.split("=");
+                    String key = sides[0];
+                    if(sides.length == 2) 
+                        if(!key.equals("") && !sides[1].equals("")) {    
+                            String[] values = sides[1].split(",");
+                            if(values.length > 0) {
+                                for(String value: values) {
+                                    entries.put(key, value);
+                                }
+                            }
+                        } else {
+                            Matmos.LOGGER.warn(String.format("%s:%d: Assignment has empty side(s)", path, lineno));
+                    } else {
+                        Matmos.LOGGER.warn(String.format("%s:%d: Invalid alias assignment. Excepted 1 '=', got %d", path, lineno, sides.length - 1));
+                    }
+                }
+                lineno++;
+            }
+        } catch (FileNotFoundException e) {
+            Matmos.LOGGER.warn("Alias file " + path + " is missing");
+        } catch (IOException e) {
+            Matmos.LOGGER.error("Error loading alias file " + path + ": " + e);
+        }
+        return entries;
+    }
 
     private void loadAliasFile(File aliasFile) {
         ConfigManager.createDefaultConfigFileIfMissing(aliasFile);
-
-        Properties props = new Properties();
-
-        try (FileReader reader = new FileReader(aliasFile)) {
-            props.load(reader);
-        } catch (FileNotFoundException e) {
-            Matmos.LOGGER.warn("Alias file (" + aliasFile.getPath() + ") is missing");
-        } catch (IOException e) {
-            Matmos.LOGGER.error("Error loading alias file (" + aliasFile.getPath() + "): " + e);
-        }
+        
+        String aliasDir = aliasFile.getParent();
+        Map<String, String> entries = loadEntries(new HashMap<String, String>(), aliasDir, aliasFile.getName(), new HashSet<String>());
 
         dealiasMap = new HashMap<Integer, Integer>();
 
-        props.stringPropertyNames().forEach(k -> Arrays.stream(props.getProperty(k).split(",")).forEach(v -> {
+        entries.entrySet().forEach(e -> {
+            String k = e.getKey();
+            String v = e.getValue();
             if (Block.blockRegistry.containsKey(k) && Block.blockRegistry.containsKey(v)) {
                 Object keyObj = Block.blockRegistry.getObject(k);
                 Object valueObj = Block.blockRegistry.getObject(v);
@@ -60,7 +109,7 @@ public class IDDealiaser {
                     dealiasMap.put(Item.getIdFromItem((Item) valueObj), Item.getIdFromItem((Item) keyObj));
                 }
             }
-        }));
+        });
     }
 
     private void compile() {
