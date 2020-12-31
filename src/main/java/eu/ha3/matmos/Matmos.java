0fb25f04be538cc2292f92d3cf2166f4e0bfe2ca
makamys <makamys@outlook.com>
2020-12-31 07:18:33 +0100

    Bump version to 1.7.10-35.3.9999

--- src/main/java/eu/ha3/matmos/Matmos.java
+++ src/main/java/eu/ha3/matmos/Matmos.java
@@ -60,7 +60,7 @@ public class Matmos extends HaddonImpl implements SupportsFrameEvents, SupportsT
 
     // Identity
     protected static final String NAME = "MAtmos";
-    protected static final String VERSION = "35.3.999";
+    protected static final String VERSION = "35.3.9999";
     protected static final String FOR = "1.7.10";
     protected static final String ADDRESS = "https://github.com/makamys/MAtmos";
     protected static final String UPDATE_JSON = "https://raw.githubusercontent.com/makamys/MAtmos/master/updatejson/update-matmos.json";
