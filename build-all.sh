d6d7bf8ef4d7c3f2f5b394fd264cb717e305f813
makamys <makamys@outlook.com>
2021-08-01 09:53:51 +0200

    Make build-all.sh clear the build/libs folder first

--- build-all.sh
+++
@@ -1,2 +0,0 @@
-( cd projects/1.7 ; gradlew build )
-( cd projects/1.12 ; gradlew build ; gradlew buildForgeMixin07 )
---
+++ clean-build-all.sh
@@ -0,0 +1,6 @@
+cleanProject () {
+	rm build/libs/*
+}
+
+( cd projects/1.7 && cleanProject && gradlew build )
+( cd projects/1.12 && cleanProject && gradlew build && gradlew buildForgeMixin07 )
