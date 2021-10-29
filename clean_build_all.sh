986f11d487ca9b4d275c4b05f3922b87da128cc2
makamys <makamys@outlook.com>
2021-10-29 08:09:32 +0200

    CI attempt 3

--- clean_build_all.sh
+++ clean_build_all.sh
@@ -2,5 +2,5 @@ cleanProject () {
 	[ ! -d build/libs ] || rm build/libs/*
 }
 
-( cd projects/1.7 && cleanProject && gradlew build )
-( cd projects/1.12 && cleanProject && gradlew build && gradlew buildForgeMixin07 )
+( cd projects/1.7 && cleanProject && ./gradlew build )
+( cd projects/1.12 && cleanProject && ./gradlew build && ./gradlew buildForgeMixin07 )
