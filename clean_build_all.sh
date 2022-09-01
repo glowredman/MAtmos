c74879668f6eb921438c86d9b57fa4e591f1cbfb
makamys <makamys@outlook.com>
2022-09-01 20:27:03 +0200

    Remove +mixin.0.7.11 build
    I realized it's unnecessary clutter, since +nomixin fixes the same problem.

--- clean_build_all.sh
+++ clean_build_all.sh
@@ -3,4 +3,4 @@ cleanProject () {
 }
 
 ( cd projects/1.7 && cleanProject && ./gradlew build && ./gradlew build -Ptarget=forge-nomixin)
-( cd projects/1.12 && cleanProject && ./gradlew build && ./gradlew build -Ptarget=forge-mixin0.7 && ./gradlew build -Ptarget=forge-nomixin )
+( cd projects/1.12 && cleanProject && ./gradlew build && ./gradlew build -Ptarget=forge-nomixin )
