fae3878026bb4edc631cfbaacb79fa5419db552c
makamys <makamys@outlook.com>
2022-09-01 19:25:20 +0200

    Build jars that don't embed Mixin

--- clean_build_all.sh
+++ clean_build_all.sh
@@ -2,5 +2,5 @@ cleanProject () {
 	[ ! -d build/libs ] || rm -f build/libs/*
 }
 
-( cd projects/1.7 && cleanProject && ./gradlew build )
-( cd projects/1.12 && cleanProject && ./gradlew build && ./gradlew buildForgeMixin07 )
+( cd projects/1.7 && cleanProject && ./gradlew build && ./gradlew build -Ptarget=forge-nomixin)
+( cd projects/1.12 && cleanProject && ./gradlew build && ./gradlew build -Ptarget=forge-mixin0.7 && ./gradlew build -Ptarget=forge-nomixin )
